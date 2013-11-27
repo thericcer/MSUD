5187aeb089cf4020708b725f8fcdd6c0b028d6f1
Ric <thericcer@gmail.com>
2013-11-27 11:58:41 -0700

    Second Revision, motor code not complete

--- Motors/Serial_Motor_Driver.ino
+++ Motors/Serial_Motor_Driver.ino
@@ -19,16 +19,32 @@
 
 #include <Servo.h>
 
-#define DRIVE 0x44
-#define STEER 0x53
-#define FORWARD 0x46
+#define DRIVE 'D'
+#define STEER 'S'
+#define FORWARD 'F'
+#define SENSOR 'Z'
 
 char MotorString[5];
+
+/* Define a Status Byte, This will be sent as a response to recieving a 5 byte command packet
+ * 
+ * 0                       7
+ * |B0|B1|B2|B3|B4|B5|B6|B7|
+ *
+ * B0 - If Set, Motors were Killed by External Interrupt, Action Needs to be taken
+ * B1 - Command Received, Values changed
+ * More to come
+ */
+char StatusByte = 0;
+char LoopCount = 0;
+
+int SensorArray[4] = {0};
 
 Servo leftFront;
 Servo leftRear;
 Servo rightFront;
 Servo rightRear;
+char newData;
 
 void setup(){
   //Begin Serial at common baud rate.
@@ -59,14 +75,14 @@ void setup(){
     from 0 to 255. 0 meaning no output and 255 being full on. The 
     pins capable of doing this are 5 and 6.
   */
-  pinMode(10, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(11, OUTPUT);
   
   //Attach Servos and set initial angle
-  leftFront.attach(6);
-  leftRear.attach(7);
-  rightFront.attach(8);
-  rightRear.attach(9);
+  leftFront.attach(7);
+  leftRear.attach(8);
+  rightFront.attach(9);
+  rightRear.attach(10);
   
   leftFront.write(0);
   leftRear.write(0);
@@ -75,79 +91,110 @@ void setup(){
 }
 
 void loop(){
+  newData = 0;
   //Serial.println("Hello");
   //Loop until we get 5 bytes from the UART then read them into MotorString
   if(Serial.available() == 5){
-    //Serial.println("In if");
+    newData = 1;
     for(int i = 0; i < 5 ; i++){
       MotorString[i] = Serial.read();
     }
+    StatusByte = 0x02 | StatusByte; 
+    Serial.write(StatusByte);
     
   }
-  
-  //Figure out what motors are called for
-  switch (MotorString[0]){
-    
-    //Drive motors called
-    case DRIVE: 
-    
-      //Set Direction, Assuming we want both front and back going the same way
-      if (MotorString[3] == FORWARD){
-        digitalWrite(2, HIGH);
-        digitalWrite(3, LOW);
-        digitalWrite(4, HIGH);
-        digitalWrite(5, LOW);
-        Serial.print("Drive Motors Set Forward");
+  if (newData){
+    //Figure out what motors are called for
+    switch (MotorString[0]){
+      
+      //Drive motors called
+      case DRIVE: 
+      
+        //Set Direction, Assuming we want both front and back going the same way
+        if (MotorString[3] == FORWARD){
+          digitalWrite(2, HIGH);
+          digitalWrite(3, LOW);
+          
+        }
+        else {
+          digitalWrite(2, LOW);
+          digitalWrite(3, HIGH);
+          
+        }
         
-      }
-      else {
-        digitalWrite(2, LOW);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, LOW);
-        digitalWrite(5, HIGH);
-        Serial.print("Drive Motors Set Reverse");
-      }
+        if (MotorString[4] == FORWARD){
+          digitalWrite(4, HIGH);
+          digitalWrite(5, LOW);
+        }
+        
+        else {
+          digitalWrite(4, LOW);
+          digitalWrite(5, HIGH);
+        }
+        
+        
+        //Next set speed
+        analogWrite(6, MotorString[1]);
+        analogWrite(11, MotorString[2]);
+        //Serial.print(" Speed L: ");
+        //Serial.print(MotorString[1], DEC);
+        //Serial.print(" R: ");
+        //Serial.println(MotorString[2], DEC);
+        break;
+        
+      case STEER:
       
-      //Next set speed
-      analogWrite(10, MotorString[1]);
-      analogWrite(11, MotorString[2]);
-      Serial.print(" Speed L: ");
-      Serial.print(MotorString[1], DEC);
-      Serial.print(" R: ");
-      Serial.println(MotorString[2], DEC);
-      break;
-      
-    case STEER:
-    
-      //Check and set servo angles
-      if(MotorString[1] <= 180){
-          leftFront.write(MotorString[1]);
-      }
-      
-      if(MotorString[2] <= 180){
-          leftRear.write(MotorString[2]);
-      }
-      
-      if(MotorString[3] <= 180){
-          rightFront.write(MotorString[3]);
-      }
-      
-      if(MotorString[4] <= 180){
-          rightRear.write(MotorString[4]);
-      }
-      
-      Serial.print("Steer Motors set to angles: ");
-      Serial.print(MotorString[1]);
-      Serial.print(MotorString[2]);
-      Serial.print(MotorString[3]);
-      Serial.println(MotorString[4]);
-              
-      break;
-      
-    default:
-      break;
+        //Check and set servo angles
+        if(MotorString[1] <= 180){
+            leftFront.write(MotorString[1]);
+        }
+        
+        if(MotorString[2] <= 180){
+            leftRear.write(MotorString[2]);
+        }
+        
+        if(MotorString[3] <= 180){
+            rightFront.write(MotorString[3]);
+        }
+        
+        if(MotorString[4] <= 180){
+            rightRear.write(MotorString[4]);
+        }
+                
+        break;
+        
+        
+      case SENSOR:
+        Serial.write(SensorArray[MotorString[1]] & 0xFF);
+        Serial.write((SensorArray[MotorString[1]] >> 8) & 0xFF);
+        break;
+        
+      default:
+        break;
+    }
   }
   
+  
+  //Update Sensor Values.
+  if(LoopCount == 1){
+    SensorArray[0] = analogRead(0);
+  }
+  if(LoopCount == 2){
+    //SensorArray[1] = 15;
+    pinMode(12, OUTPUT);
+    digitalWrite(12, LOW);
+    delayMicroseconds(2);
+    digitalWrite(12, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(12, LOW);
+    pinMode(12, INPUT);    
+    SensorArray[1] = (pulseIn(12, HIGH)/29)/2;
+    
+    //Serial.println(SensorArray[1], DEC);
+    LoopCount = 0;
+  }
+  
+  LoopCount++;
 }
   
 
